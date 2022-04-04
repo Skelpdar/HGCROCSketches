974625fee4c1ef229e4f6117cf322ab573a6c862
Alexander Ekman <per.alexander.ekman@cern.ch>
2022-04-04 15:28:26 +0200

    improved charge injection 2D plots

--- plot/plot_scancharge.py
+++ plot/plot_scancharge.py
@@ -9,10 +9,20 @@ from matplotlib.image import NonUniformImage
 
 def plot_pulse(data,dpm=1,odir='./'):
     links = list(data["ILINK"].unique())
-    calib_dacs = list(data['CALIB_DAC'].unique())
+    calib_dacs = [min(list(data['CALIB_DAC'].unique())),max(list(data['CALIB_DAC'].unique()))]
 
-    fig, axs = plt.subplots(len(links), len(calib_dacs))
-    fig.suptitle('Channel vs sample, dpm1 charge injection (ILINK:CALIB_DAC)')
+    #fig, axs = plt.subplots(len(links), len(calib_dacs), figsize=(8.27, 11.69), dpi=100)
+
+    
+    fig = plt.figure(figsize=(8.27, 11.69), dpi=100)
+    #fig.suptitle('Channel vs sample, dpm1 charge injection (ILINK:CALIB_DAC)')
+    fig.suptitle(f'dpm{dpm} Charge Injection')
+    #fig.supxlabel("Charge Strength [DAC]")
+    #fig.supylabel("ELink Number")
+    gs = fig.add_gridspec(len(links), len(calib_dacs), hspace=0.2, wspace=0.05)
+    axs = gs.subplots(sharex='col', sharey='row')
+    
+    print(axs[0][0])
     
     for ilink_counter,ilink in enumerate(links):
         for calib_dac_counter,calib_dac in enumerate(calib_dacs):
@@ -25,13 +35,14 @@ def plot_pulse(data,dpm=1,odir='./'):
             weights=[]
             
             for index, row in df.iterrows():
-                for sample in ["ADC0","ADC1","ADC2","ADC3"]:
+                for sample in ["ADC0","ADC1","ADC2","ADC3","ADC4","ADC5","ADC6","ADC7"]:
+                #for sample in ["ADC0","ADC1","ADC2","ADC3"]:
                     x.append(int(sample[3]))
                     y.append(index)
                     weights.append(int(row[sample]))
 
             xmin = 0
-            xmax = 4
+            xmax = 8
             xedges = np.linspace(xmin, xmax, xmax+1)
             
             ymin = 0
@@ -43,11 +54,23 @@ def plot_pulse(data,dpm=1,odir='./'):
             # Histogram does not follow Cartesian convention, therefore transpose H for visualization purposes.
             H = H.T
 
-            axs[ilink_counter, calib_dac_counter].pcolormesh(X, Y, H)
-            axs[ilink_counter, calib_dac_counter].xaxis.set_ticks([0,1,2,3,4])
-            axs[ilink_counter, calib_dac_counter].yaxis.set_ticks([0,18,36])
-            axs[ilink_counter, calib_dac_counter].set_title(f"{ilink} : {calib_dac}")
-            
+            print(ilink_counter,calib_dac_counter)
+            axs[ilink_counter][calib_dac_counter].pcolormesh(X, Y, H)
+            axs[ilink_counter][calib_dac_counter].xaxis.set_ticks([0,1,2,3,4,5,6,7,8])
+            axs[ilink_counter][calib_dac_counter].yaxis.set_ticks([0,18,36])
+            if ilink_counter == 0:
+                ax = axs[ilink_counter][calib_dac_counter].twiny()
+                ax.set_xlabel(f"Charge Strength: {calib_dacs[calib_dac_counter]} [DAC]")
+                ax.set_xticklabels([])
+            if calib_dac_counter == len(calib_dacs)-1:
+                ax = axs[ilink_counter][calib_dac_counter].twinx()
+                ax.set_ylabel(f"Elink: {ilink_counter}")
+                ax.set_yticklabels([])
+            if ilink_counter == len(links)-1:
+                axs[ilink_counter][calib_dac_counter].set_xlabel(f"Time Sample")
+            if calib_dac_counter == 0:
+                axs[ilink_counter][calib_dac_counter].set_ylabel(f"Channel")
+                
     plt.tight_layout()
     plt.savefig(f"{odir}/pulses_dpm{dpm}.pdf")
 
@@ -96,7 +119,7 @@ def plot_calibdac_amplitude(data,dpm=1,odir='./'):
 def main(arg):
     data = pd.read_csv(arg.fname)
     plot_pulse(data,arg.dpm,arg.odir)
-    plot_calibdac_amplitude(data,arg.dpm,arg.odir)
+    #plot_calibdac_amplitude(data,arg.dpm,arg.odir)
         
 if __name__=="__main__":
     parser = argparse.ArgumentParser(f'python plot_scancharge.py ',
